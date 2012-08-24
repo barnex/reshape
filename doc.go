43ace57be8009218f79f819508f11a0de10b31af
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-08-24 10:24:30 +0200

    minor

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
 /*
  This package converts between 1D arrays (like []float32) and
  mulit-dimensional arrays (like [][][]float32),
- while sharing underlying storage.
-
+ sharing underlying storage.
  Also allows to re-interpret arrays.
- E.g., cast a []complex128 as a []float64 of twice the size. 
+ E.g., cast a []complex128 as a []float64 of twice the size,
+ sharing underlying storage.
 
  This is often needed when passing multi-dimensional
  arrays as contigous lists to, e.g., C code.
